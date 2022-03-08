496e10ae39bb1b5e9bf3e4b6d6d7e8b01d41f881
Bissbert <43237892+Bissbert@users.noreply.github.com>
2022-03-08 22:50:02 +0100

    sonarqube test

--- src/test/java/ch/bissbert/galleryprojectserver/data/ImageTest.java
+++ src/test/java/ch/bissbert/galleryprojectserver/data/ImageTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 public class ImageTest {
@@ -13,5 +14,14 @@ public class ImageTest {
         Image id1Image = new Image();
         id1Image.setId(1);
         assertEquals(id1Image, Image.builder().id(1).build());
+        assertNotEquals(id1Image, null);
+        assertNotEquals(id1Image, "null");
+    }
+
+    @Test
+    public void noConstructorCrash() {
+        Image image1 = new Image(1, 10, 10, 10, "Test", "testname", "descr", ImageMimeType.builder().build());
+        Image image2 = new Image(2, new byte[10], ImageMimeType.builder().build());
+        assertNotEquals(image1.getId(), image2.getId());
     }
 }
