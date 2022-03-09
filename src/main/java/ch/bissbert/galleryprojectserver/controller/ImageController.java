c98a922aecea05a6d238dcfe228e86180c7f523c
goncalvesmBZZ <goncalvesm@bzz.ch>
2022-03-09 23:14:07 +0100

    getMetaData, not finished with catches and alignment

--- src/main/java/ch/bissbert/galleryprojectserver/controller/ImageController.java
+++ src/main/java/ch/bissbert/galleryprojectserver/controller/ImageController.java
@@ -4,20 +4,41 @@ import ch.bissbert.galleryprojectserver.ImageUtil;
 import ch.bissbert.galleryprojectserver.data.Image;
 import ch.bissbert.galleryprojectserver.repo.ImageMimeTypeRepository;
 import ch.bissbert.galleryprojectserver.repo.ImageRepository;
+import com.drew.imaging.ImageMetadataReader;
+import com.drew.imaging.ImageProcessingException;
+import com.drew.imaging.gif.GifMetadataReader;
+import com.drew.imaging.jpeg.JpegMetadataReader;
+import com.drew.imaging.jpeg.JpegProcessingException;
+import com.drew.imaging.png.PngMetadataReader;
+import com.drew.imaging.tiff.TiffMetadataReader;
+import com.drew.metadata.Directory;
+import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
 import org.apache.commons.imaging.ImageReadException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Controller for the image resource.
@@ -93,6 +114,82 @@ public class ImageController {
         images.stream().map(Image::toString).forEach(s -> logger.info(s));
         return images;
     }
+
+    @GetMapping(value = "images/metadata")
+    @ResponseBody
+    @CrossOrigin(origins = "http://localhost:3000")
+    public ResponseEntity getMetaData(@RequestParam int id) {
+        Image image = imageRepository.getById(id);
+        byte[] file = getCSV(image);
+            return ResponseEntity.ok()
+                    .header("Content-Disposition", "attachment; filename=" + image.getName() + ".csv")
+                    .contentType(MediaType.parseMediaType("text/csv"))
+                    .body(file);
+    }
+
+    public  byte[] getCSV(Image image){
+            Metadata metaData = null;
+            ByteArrayInputStream stream = new ByteArrayInputStream(image.getFullImage());
+            try {
+                switch (image.getCompressionType()) {
+                    case "JPEG":
+
+                        metaData = JpegMetadataReader.readMetadata(stream);
+                        break;
+                    case "PNG":
+                        metaData = PngMetadataReader.readMetadata(stream);
+                        break;
+                    case "GIF":
+                        metaData = GifMetadataReader.readMetadata(stream);
+                        break;
+                    case "TIFF":
+                        metaData = TiffMetadataReader.readMetadata(stream);
+                        break;
+                    default:
+                        metaData = ImageMetadataReader.readMetadata(stream);
+                }
+            }
+            catch (JpegProcessingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ImageProcessingException e) {
+                e.printStackTrace();
+            }
+            FileOutputStream fileOut;
+            File myFile = new File(image.getName() + "metaData" + ".csv");
+
+            try(Workbook workbook = new XSSFWorkbook()) {
+                for (Directory directory : metaData.getDirectories()) {
+                    Sheet sheet = workbook.createSheet(directory.getName());
+                    int rowCount = 0;
+                    for (Tag tag : directory.getTags()) {
+                        System.out.format("[%s] - %s = %s",
+                                directory.getName(), tag.getTagName(), tag.getDescription());
+                        Row row = sheet.createRow(rowCount);
+                        rowCount++;
+                        Cell cell = row.createCell(0);
+                        cell.setCellValue(tag.getTagName());
+                        Cell cell2 = row.createCell(1);
+                        cell2.setCellValue(tag.getDescription());
+                    }
+                }
+
+
+                try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+                    workbook.write(outputStream);
+                    workbook.close();
+                    return outputStream.toByteArray();
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
 
     /**
      * A spring boot rest service that serves a single image as a byte array.
